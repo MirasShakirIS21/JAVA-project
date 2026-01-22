1a927bfff8a0c6ea02e0db6780d1c5bf156e2fd5
MirasShakirIS21 <mirassakirov395@gmail.com>
2026-01-22 14:21:10 +0500

    Add files via upload

---
+++ StudentInfo.java
@@ -0,0 +1,12 @@
+public class StudentInfo {
+
+    public static void main(String[] args) {
+
+        System.out.println("=== Информация о студенте ===");
+        System.out.println("ФИО: Иванов Иван Иванович");
+        System.out.println("Группа: ИС-2204");
+        System.out.println("Специальность: Информационные системы");
+
+    }
+}
+
